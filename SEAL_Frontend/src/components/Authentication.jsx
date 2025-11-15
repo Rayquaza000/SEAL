b925b2bbdc74b09a99b99ecc9e253f47c6e16342
Rayquaza000 <mehtapratik2001@gmail.com>
2025-11-15 18:33:50 +0530

    Started with frontend. Created header

---
+++ SEAL_Frontend/src/components/Authentication.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authentication = () => {
+  return (
+    <div className='w-full h-fit flex flex-col'>
+        <div className='w-fit h-fit p-5 border-solid border-1px border-[#948A54] rounded-[10px]'>No saved emails</div>
+    </div>
+  )
+}
+
+export default Authentication
