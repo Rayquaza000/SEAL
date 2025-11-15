7502a4b507823c9e4e28d7b826001a40762d4d77
Rayquaza000 <mehtapratik2001@gmail.com>
2025-11-15 18:41:41 +0530

    bug fixing

--- SEAL_Frontend/src/App.jsx
+++ SEAL_Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import { Outlet } from 'react-router-dom';
 import Authentication from './components/Authentication';
@@ -17,10 +17,10 @@ function App() {
 ]);
 
   return (
-    <>
+    <RouterProvider router={router}>
       <Header/>
       <Outlet/>
-    </>
+    </RouterProvider>
   )
 }
 
